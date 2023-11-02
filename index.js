791a5418a32ea941affa14428c540a8058ae7860
Vladislav Bolofinov <105604362+VladBolofinov@users.noreply.github.com>
2023-11-02 14:06:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-const jsonServer = require("json-server"); // importing json-server library
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-server.use(middlewares);
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
+server.use(async (req, res, next) => {
+    await new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+    next();
+});
+
+server.post('/login', (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const { users = [] } = db;
+
+        const userFromBd = users.find(
+            (user) => user.username === username && user.password === password,
+        );
+
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+
+        return res.status(403).json({ message: 'User not found' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: e.message });
+    }
+});
+
+server.use((req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: 'AUTH ERROR' });
+    }
+
+    next();
+});
+
 server.use(router);
-
-server.listen(port);
+server.listen(8000, () => {
+    console.log('server is running on 8000 port');
+});
